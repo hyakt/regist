78d75600517bc4a4d435fb96e8280ca2ea0b328f
hyakt <hyakt0@gmail.com>
2019-06-19 00:40:18 +0900

    add: Native Base

--- App.js
+++ App.js
@@ -33,12 +33,11 @@ async function loadResourcesAsync() {
       require('app/src/assets/images/robot-dev.png'),
       require('app/src/assets/images/robot-prod.png'),
     ]),
+
     Font.loadAsync({
-      // This is the font that we are using for our tab bar
-      ...Ionicons.font,
-      // We include SpaceMono because we use it in HomeScreen.js. Feel free to
-      // remove this if you are not using it in your app
-      'space-mono': require('app/src/assets/fonts/SpaceMono-Regular.ttf'),
+  'Roboto': require('native-base/Fonts/Roboto.ttf'),
+    'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+    ...Ionicons.font,
     }),
   ]);
 }
