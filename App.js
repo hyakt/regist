33afd66ab97139395e44b9d4429e61db921e5a34
hyakt <kaji1216@gmail.com>
2019-06-20 01:21:02 +0900

    update: Asset

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-import { AppLoading, Asset } from 'expo'
+import { AppLoading } from 'expo'
+import { Asset } from 'expo-asset'
 import * as Font from 'expo-font'
 import React, { useState } from 'react'
 import { Platform, StatusBar, StyleSheet, View } from 'react-native'
