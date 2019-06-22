f26c6d74b3ff3fe3219dede90d3d2841da4147de
hyakt <hyakt0@gmail.com>
2019-06-22 17:18:44 +0900

    update: HomeScreen

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
-import { AppLoading, Asset } from 'expo';
-import * as Font from 'expo-font';
-import React, { useState } from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import { AppLoading } from 'expo'
+import { Asset } from 'expo-asset'
+import * as Font from 'expo-font'
+import React, { useState } from 'react'
+import { Platform, StatusBar, StyleSheet, View } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
 
-import AppNavigator from 'app/src/navigation/AppNavigator';
+import AppNavigator from 'app/src/navigation/AppNavigator'
 
-export default function App(props) {
-  const [isLoadingComplete, setLoadingComplete] = useState(false);
+export default function App (props) {
+  const [isLoadingComplete, setLoadingComplete] = useState(false)
 
   if (!isLoadingComplete && !props.skipLoadingScreen) {
     return (
@@ -16,45 +17,45 @@ export default function App(props) {
         onError={handleLoadingError}
         onFinish={() => handleFinishLoading(setLoadingComplete)}
       />
-    );
+    )
   } else {
     return (
       <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+        {Platform.OS === 'ios' && <StatusBar barStyle='default' />}
         <AppNavigator />
       </View>
-    );
+    )
   }
 }
 
-async function loadResourcesAsync() {
+async function loadResourcesAsync () {
   await Promise.all([
     Asset.loadAsync([
       require('app/src/assets/images/robot-dev.png'),
-      require('app/src/assets/images/robot-prod.png'),
+      require('app/src/assets/images/robot-prod.png')
     ]),
 
     Font.loadAsync({
-  'Roboto': require('native-base/Fonts/Roboto.ttf'),
-    'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
-    ...Ionicons.font,
-    }),
-  ]);
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+      ...Ionicons.font
+    })
+  ])
 }
 
-function handleLoadingError(error: Error) {
+function handleLoadingError (error: Error) {
   // In this case, you might want to report the error to your error reporting
   // service, for example Sentry
-  console.warn(error);
+  console.warn(error)
 }
 
-function handleFinishLoading(setLoadingComplete) {
-  setLoadingComplete(true);
+function handleFinishLoading (setLoadingComplete) {
+  setLoadingComplete(true)
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-  },
-});
+    backgroundColor: '#fff'
+  }
+})
