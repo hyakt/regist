feee0cffd51386babcc997cfa278a8b67fd930c5
hyakt <kaji1216@gmail.com>
2019-07-02 15:35:48 +0900

    update: navigator

--- src/navigation/MainNavigator.js
+++ src/navigation/MainNavigator.js
@@ -1,16 +1,25 @@
 import React from 'react'
-import { createDrawerNavigator } from 'react-navigation'
+import { createDrawerNavigator, createStackNavigator } from 'react-navigation'
 
 import HomeScreen from '../screens/HomeScreen'
 import ArticleScreen from '../screens/ArticleScreen'
 import SideBar from '../screens/SideBar'
 
-export default createDrawerNavigator(
+const drawerNavigator = createDrawerNavigator(
   {
-    Home: { screen: HomeScreen },
-    Article: { screen: ArticleScreen }
+    Home: { screen: HomeScreen }
   },
   {
     contentComponent: props => <SideBar {...props} />
+  }
+)
+
+export default createStackNavigator(
+  {
+    Home: { screen: drawerNavigator },
+    Article: { screen: ArticleScreen }
+  },
+  {
+    headerMode: 'none'
   }
 )
