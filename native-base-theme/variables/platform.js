33b60ed74fc9a9dfa09de5e028516096bd25352a
hyakt <kaji1216@gmail.com>
2019-07-08 09:15:14 +0900

    update: color

--- native-base-theme/variables/platform.js
+++ native-base-theme/variables/platform.js
@@ -142,20 +142,20 @@ export default {
   // FooterTab
   tabBarTextColor: platform === "ios" ? "#6b6b6b" : "#b3c7f9",
   tabBarTextSize: platform === "ios" ? 14 : 11,
-  activeTab: platform === "ios" ? "#007aff" : "#fff",
-  sTabBarActiveTextColor: "#007aff",
-  tabBarActiveTextColor: platform === "ios" ? "#007aff" : "#fff",
+  activeTab: platform === "ios" ? "#a6e22e" : "#fff",
+  sTabBarActiveTextColor: "#a6e22e",
+  tabBarActiveTextColor: platform === "ios" ? "#a6e22e" : "#fff",
   tabActiveBgColor: platform === "ios" ? "#cde1f9" : "#3F51B5",
 
   // Header
-  toolbarBtnColor: platform === "ios" ? "#007aff" : "#fff",
+  toolbarBtnColor: platform === "ios" ? "#a6e22e" : "#fff",
   toolbarDefaultBg: platform === "ios" ? "#272822" : "#3F51B5",
   toolbarHeight: platform === "ios" ? 64 : 56,
   toolbarSearchIconSize: platform === "ios" ? 20 : 23,
   toolbarInputColor: platform === "ios" ? "#CECDD2" : "#fff",
   searchBarHeight: platform === "ios" ? 30 : 40,
   searchBarInputHeight: platform === "ios" ? 30 : 50,
-  toolbarBtnTextColor: platform === "ios" ? "#007aff" : "#fff",
+  toolbarBtnTextColor: platform === "ios" ? "#a6e22e" : "#fff",
   toolbarDefaultBorder: platform === "ios" ? "#a7a6ab" : "#3F51B5",
   iosStatusbar: "light-content",
   get statusBarColor() {
@@ -193,7 +193,7 @@ export default {
   lineHeightH2: 27,
   lineHeightH3: 22,
   lineHeight: platform === "ios" ? 20 : 24,
-  listItemSelected: platform === "ios" ? "#007aff" : "#3F51B5",
+  listItemSelected: platform === "ios" ? "#a6e22e" : "#3F51B5",
 
   // List
   listBg: "transparent",
@@ -218,10 +218,10 @@ export default {
 
   // Segment
   segmentBackgroundColor: platform === "ios" ? "#272822" : "#3F51B5",
-  segmentActiveBackgroundColor: platform === "ios" ? "#007aff" : "#fff",
-  segmentTextColor: platform === "ios" ? "#007aff" : "#fff",
+  segmentActiveBackgroundColor: platform === "ios" ? "#a6e22e" : "#fff",
+  segmentTextColor: platform === "ios" ? "#a6e22e" : "#fff",
   segmentActiveTextColor: platform === "ios" ? "#fff" : "#3F51B5",
-  segmentBorderColor: platform === "ios" ? "#007aff" : "#fff",
+  segmentBorderColor: platform === "ios" ? "#a6e22e" : "#fff",
   segmentBorderColorMain: platform === "ios" ? "#a7a6ab" : "#3F51B5",
 
   // Spinner
@@ -231,9 +231,9 @@ export default {
   // Tab
   tabDefaultBg: platform === "ios" ? "#F8F8F8" : "#3F51B5",
   topTabBarTextColor: platform === "ios" ? "#6b6b6b" : "#b3c7f9",
-  topTabBarActiveTextColor: platform === "ios" ? "#007aff" : "#fff",
+  topTabBarActiveTextColor: platform === "ios" ? "#a6e22e" : "#fff",
   topTabBarBorderColor: platform === "ios" ? "#a7a6ab" : "#fff",
-  topTabBarActiveBorderColor: platform === "ios" ? "#007aff" : "#fff",
+  topTabBarActiveBorderColor: platform === "ios" ? "#a6e22e" : "#fff",
 
   // Tabs
   tabBgColor: "#F8F8F8",
