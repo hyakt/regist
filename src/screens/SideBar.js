1a2464364c4309c233da4a11d94bbfe3f6761efc
hyakt <kaji1216@gmail.com>
2019-07-10 18:55:05 +0900

    add: revoke function

--- src/screens/SideBar.js
+++ src/screens/SideBar.js
@@ -8,37 +8,47 @@ import platform from 'app/native-base-theme/variables/platform'
 
 export default (props) => {
   const [user, setUser] = useState(null)
+  const [token, setToken] = useState(null)
+  const [itemList, setItemList] = useState([])
   const { navigation } = props
 
   useEffect(() => {
     setUser(navigation._childrenNavigation.Home.getParam('user'))
   }, [navigation._childrenNavigation.Home.getParam('user')])
 
-  const itemList = [
-    { 'name': 'Sign Out',
-      'f': async () => {
-        // github.revokeAuthentication()
-      await AsyncStorage.clear()
-      await navigation.navigate('AuthLoading')
-    } }
-  ]
+  useEffect(() => {
+    setToken(navigation._childrenNavigation.Home.getParam('token'))
+  }, [navigation._childrenNavigation.Home.getParam('token')])
+
+  useEffect(() => {
+    console.log(token)
+    setItemList([
+      { 'name': 'Sign Out',
+        'f': async () => {
+          console.log(await github.revokeAuthorization(token))
+          await AsyncStorage.clear()
+          await navigation.navigate('AuthLoading')
+        }
+      }
+    ])
+    }, [token])
 
   return (
     <StyleProvider style={getTheme(platform)}>
       <Container>
         <Content>
           {user
-            ? <View style={styles.profileContainer}>
-              <Image
-                style={styles.icon}
-                source={{ uri: user.avatar_url }}
-              />
-              <View style={styles.nameContainer}>
-                <Text style={styles.name}>{user.name}</Text>
-                <Text style={styles.loginName}>{user.login}</Text>
-              </View>
+           ? <View style={styles.profileContainer}>
+            <Image
+              style={styles.icon}
+              source={{ uri: user.avatar_url }}
+            />
+            <View style={styles.nameContainer}>
+              <Text style={styles.name}>{user.name}</Text>
+              <Text style={styles.loginName}>{user.login}</Text>
             </View>
-            : <View />
+          </View>
+           : <View />
           }
           <FlatList
             data={itemList}
